4c99da2a1a096c676a7532c1dba9eece27c24f39
Dinesh Rabara <dinesh.rabara@gmail.com>
2015-06-03 15:35:54 +0530

    add form tags

--- src/Dinesh/Easyform/EasyForm.php
+++ src/Dinesh/Easyform/EasyForm.php
@@ -43,6 +43,90 @@ class EasyForm {
         return $this;
     }
 
+    public function input($type, $name, $value = null, $options = array()) {
+        $this->tags['{{tag_name}}'] = $name;
+        $this->tags['{{tag}}'] = Form::input($type, $name, $value, $options);
+        return $this;
+    }
+
+    public function password($name, $options = array()) {
+        $this->tags['{{tag_name}}'] = $name;
+        $this->tags['{{tag}}'] = Form::password($name, $options);
+        return $this;
+    }
+
+    public function email($name, $value = null, $options = array()) {
+        $this->tags['{{tag_name}}'] = $name;
+        $this->tags['{{tag}}'] = Form::email($name, $value, $options);
+        return $this;
+    }
+
+    public function url($name, $value = null, $options = array()) {
+        $this->tags['{{tag_name}}'] = $name;
+        $this->tags['{{tag}}'] = Form::url($name, $value, $options);
+        return $this;
+    }
+
+    public function file($name, $options = array()) {
+        $this->tags['{{tag_name}}'] = $name;
+        $this->tags['{{tag}}'] = Form::file($name, $options);
+        return $this;
+    }
+
+    public function textarea($name, $value = null, $options = array()) {
+        $this->tags['{{tag_name}}'] = $name;
+        $this->tags['{{tag}}'] = Form::textarea($name, $value, $options);
+        return $this;
+    }
+
+    public function number($name, $value = null, $options = array()) {
+        $this->tags['{{tag_name}}'] = $name;
+        $this->tags['{{tag}}'] = Form::number($name, $value, $options);
+        return $this;
+    }
+
+    public function select($name, $list = array(), $selected = null, $options = array()) {
+        $this->tags['{{tag_name}}'] = $name;
+        $this->tags['{{tag}}'] = Form::select($name, $list, $selected, $options);
+        return $this;
+    }
+
+    public function selectRange($name, $begin, $end, $selected = null, $options = array()) {
+        $this->tags['{{tag_name}}'] = $name;
+        $this->tags['{{tag}}'] = Form::selectRange($name, $begin, $end, $selected, $options);
+        return $this;
+    }
+
+    public function checkbox($name, $value = 1, $checked = null, $options = array()) {
+        $this->tags['{{tag_name}}'] = $name;
+        $this->tags['{{tag}}'] = Form::checkbox($name, $value, $checked, $options);
+        return $this;
+    }
+
+    public function radio($name, $value = null, $checked = null, $options = array()) {
+        $this->tags['{{tag_name}}'] = $name;
+        $this->tags['{{tag}}'] = Form::radio($name, $value, $checked, $options);
+        return $this;
+    }
+
+    public function image($url, $name = null, $attributes = array()) {
+        $this->tags['{{tag_name}}'] = $name;
+        $this->tags['{{tag}}'] = Form::image($url, $name, $attributes);
+        return $this;
+    }
+
+    public function submit($value = null, $options = array()) {
+        $this->tags['{{tag_name}}'] = $value;
+        $this->tags['{{tag}}'] = Form::submit($value, $options);
+        return $this;
+    }
+
+    public function button($value = null, $options = array()) {
+        $this->tags['{{tag_name}}'] = $value;
+        $this->tags['{{tag}}'] = Form::button($value, $options);
+        return $this;
+    }
+
     public function error($errors, $message = '<span class="error">:message</span>') {
         $this->tags['{{error-first}}'] = $errors->first($this->tags['{{tag_name}}'], $message);
         $this->tags['{{error-has}}'] = $errors->has($this->tags['{{tag_name}}']) ? 'has-error' : '';
